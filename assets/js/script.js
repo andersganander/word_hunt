e05d751e502860a9e373049bbc59ca12fb531545
Anders Ganander <anders.ganander@gmail.com>
2024-04-08 18:55:08 +0000

    Add style for board_area and letter_square #6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+// Handle event DOMContentLoaded
+document.addEventListener('DOMContentLoaded', startGame());
+
+/**
+ * Function to be called when start game button is clicked
+ * 
+ */
 function startGame(){
+    // Pick a random word
+    const nineletterWords = ["ADVENTURE", "BRILLIANT", "CHOCOLATE", "DANGEROUS", "EDUCATION"];
+    const pickedWord = nineletterWords[Math.floor(Math.random() * nineletterWords.length)]
+
+    // Put letters in board area (unrandomly now... will be changed to random)
+    const squares = document.getElementsByClassName('letter_square');
+    for (let i = 0; i<squares.length; i++) {
+        let square = squares[i];
+        square.textContent=pickedWord[i];
+    }
+
+    // Start timer and show on screen
+
+    // Update score 
     console.log("Game started...")
 }
+
